c85cbbf80636b98170e73b44594f4124ed602c67
Arcega <custodiojohn34@gmail.com>
2024-09-11 14:23:52 +0800

    division method

--- calculator1.java
+++ calculator1.java
@@ -12,12 +12,22 @@ public class Calculator {
         return a * b;
     }
 
+    public static double division(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            throw new ArithmeticException("Division by zero");
+        }
+    }
+
     
+
     public static void main(String[] args) {
         // Example usage of the calculator methods
         System.out.println("Addition: " + addition(5, 3));
         System.out.println("Subtraction: " + subtraction(5, 3));
         System.out.println("Multiplication: " + multiplication(5, 3));
-       
+        System.out.println("Division: " + division(5, 3));
+        
     }
 }
