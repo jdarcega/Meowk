161c53755e28a644eca4fdd5a6cb336935061413
Arcega <custodiojohn34@gmail.com>
2024-09-11 14:14:39 +0800

    addition method

---
+++ calculator1.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    
+
+    public static void main(String[] args) {
+        // Example usage of the calculator methods
+        System.out.println("Addition: " + addition(5, 3));
+        
+    }
+}
